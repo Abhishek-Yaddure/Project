311f58229be6ec7c29764ca350e9181783a963e1
Abhishek <abhishekyaddure12345@gmail.com>
2023-09-27 16:47:07 +0530

    message

--- src/main/java/com/project/project/exception/GlobalException.java
+++ src/main/java/com/project/project/exception/GlobalException.java
@@ -1,14 +1,19 @@
 package com.project.project.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
-public class GlobalException {
+public class GlobalException extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BadCredentialException.class)
     public ResponseEntity<String> handleBadcradentialException(){
@@ -19,6 +24,14 @@ public class GlobalException {
     public ResponseEntity<String>handleNosuchArgumentException(){
         return new ResponseEntity<>("No value present in DB with this Id,please change your request",HttpStatus.BAD_REQUEST);
     }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+                                                                         HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        return new ResponseEntity<Object>("please change mehod type",HttpStatus.NOT_FOUND);
+    }
+
 }
 
 
